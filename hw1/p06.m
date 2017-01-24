55580e01d7046e79bf6bb3c71c8bed7563ea3ade
David Zhu <hdavidzhu@gmail.com>
2017-01-24 10:45:24 -0500

    ADD: p06.

--- hw1/p06.m
+++ hw1/p06.m
@@ -3,31 +3,26 @@ x = -5:0.01:5;
 close all;
 hold on;
 
-u_single = @(t, x, k, n) (Sigma(t, x - (k/(n+1))) - Sigma(t, x + (k/(n+1))));
+u_single = @(t, x, k, n) (1/(2*k-1)) * (e.^(-(2*k-1)^2 * t)) * sin((2*k-1)*x);
 
 n = 1000;
 
 t = 0.00001;
 u = 0; for k = 1:n u += u_single(t, x, k, n); endfor
-u /= n;
 plot(x, u);
 
 t = 0.1;
 u = 0; for k = 1:n u += u_single(t, x, k, n); endfor
-u /= n;
 plot(x, u);
 
 t = 0.2;
 u = 0; for k = 1:n u += u_single(t, x, k, n); endfor
-u /= n;
 plot(x, u);
 
 t = 1;
 u = 0; for k = 1:n u += u_single(t, x, k, n); endfor
-u /= n;
 plot(x, u);
 
 t = 2;
 u = 0; for k = 1:n u += u_single(t, x, k, n); endfor
-u /= n;
 plot(x, u);
