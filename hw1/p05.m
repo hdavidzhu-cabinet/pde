1c9d26bf2e41da5ba03452d6d456a8d0db925fbb
David Zhu <hdavidzhu@gmail.com>
2017-01-24 01:19:26 -0500

    SIMPLIFY: Input equation.

--- hw1/p05.m
+++ hw1/p05.m
@@ -3,18 +3,29 @@ x = -5:0.01:5;
 close all;
 hold on;
 
+u_single = @(t, x, k, n) (Sigma(t, x - (k/(n+1))) - Sigma(t, x + (k/(n+1))));
+
+n = 1000;
+
 t = 0.1;
-u = (1/2)*(Sigma(t, x - (1/2)) - Sigma(t, x + (1/2)));
+u = 0; for k = 1:n
+u += u_single(t, x, k, n);
+% disp(max(u))
+endfor
+u /= n;
 plot(x, u);
 
 t = 0.2;
-u = (1/2)*(Sigma(t, x - (1/2)) - Sigma(t, x + (1/2)));
+u = 0; for k = 1:n u += u_single(t, x, k, n); endfor
+u /= n;
 plot(x, u);
 
 t = 1;
-u = (1/2)*(Sigma(t, x - (1/2)) - Sigma(t, x + (1/2)));
+u = 0; for k = 1:n u += u_single(t, x, k, n); endfor
+u /= n;
 plot(x, u);
 
 t = 2;
-u = (1/2)*(Sigma(t, x - (1/2)) - Sigma(t, x + (1/2)));
+u = 0; for k = 1:n u += u_single(t, x, k, n); endfor
+u /= n;
 plot(x, u);
